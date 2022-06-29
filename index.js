a0ebe79437a24695b4a443b09d8c1dde2176c872
quantium-rock <alexcollsoutumuro@gmail.com>
2022-06-29 14:53:20 +0200

    added Stand

--- index.js
+++ index.js
@@ -2,83 +2,131 @@ const btnNew = $('.btn-new');
 const btnHit = $('.btn-hit');
 const btnStand = $('.btn-stand');
 
-
-
 let card;
-let score = 0;
 let dealerScore = 0;
 let playerScore = 0;
+let dealerAces = 0;
+let playerAces = 0;
 
-function prepareDeck() {
-
+function initDeck() {
     const cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
-    const suits = ['C','D','H','S'];
+    const suits = ['C','D','H','S']; // Clubs, Diamonds, Hearts, Shapes
     let deck = [];
-    
+    // Build the deck
     for(let c of cards ){
         for(let s of suits){
             deck.push(c+s);
         }
     }
-
+    // Mix randomly the deck before returning
     return deck.sort( () => Math.random() - 0.5 );
 }
 
-let deck = prepareDeck();
+let deck = initDeck();
     
 
 function cardScore ( card, currentScore ) {
     let value = card.slice(0,-1);
     const ten = ['J','Q','K'];
     if ( ten.includes(value) ) {
-        value = 10;
+        return 10;
     } else if ( value === 'A' ) {
         if ( currentScore + 11 > 21 ) {
-            value = 1;
+            return 1;
         } else {
-            value = 11;
+            return 11;
         }
     } else {
-        value = Number(value);
+        return Number(value);
     }
-    return value;   
 }
 
-function hitCard () {
+
+function cardDealer () {
+    card = deck.shift();
+    $('.dealer').html($('.dealer').html() + `<img src="./cards/${card}.svg" alt="">`);
+    dealerScore += cardScore(card, dealerScore);
+    $('.dealer-score').text(dealerScore);
+}
+
+function cardPlayer () {
     card = deck.shift();
     $('.player').html($('.player').html() + `<img src="./cards/${card}.svg" alt="">`);
     playerScore += cardScore(card, playerScore);
     $('.player-score').text(playerScore);
 }
 
-function newCard ( card ) {
-    $('.dealer').html($('.dealer').html() + `<img src="./cards/${card}.svg" alt="">`);
-}
-
 function newGame () {
-    $('.dealer').html('');
+    // Reset All
+    $('.dealer').empty();
     dealerScore = 0;
-    $('.dealerScore').text(dealerScore);
-    $('.player').html('');
-    playerScore = 0;
-    $('.playerScore').text(playerScore);
-
-    card = deck.shift();
-    newCard(card);
-    newCard('0');
-    dealerScore += cardScore(card, dealerScore);
     $('.dealer-score').text(dealerScore);
+    $('.player').empty();
+    playerScore = 0;
+    $('.player-score').text(playerScore);
+
+    // Initialize deck
+    deck = initDeck();
+
+    cardDealer();
+    $('.dealer').html($('.dealer').html() + '<img class="0" src="./cards/0.svg" alt="">');
     
-    
+    cardPlayer();
+    cardPlayer();
+    btnHit.attr('disabled',false);
+    btnStand.attr('disabled',false);
 
 }
 
 newGame();
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+async function dealerPlays() {
+    btnHit.attr('disabled',true);
+    $('.0').remove();
+    let more = true;
+    while ( more ) {
+        cardDealer();
+        if ( dealerScore >= playerScore || dealerScore >= 21 ) {
+            more = false;
+            btnStand.attr('disabled',true);
+        }
+        await sleep(1000);
+    }    
+}
+
 btnNew.click( function () {
     newGame();
 })
 
+function youWin () {
+    $('.message').removeClass('hidden');
+    $('.message').addClass('back-green');
+}
+
+function youBust () {
+
+}
+
+function houseWins () {
+
+}
+
 btnHit.click( function () {
-    hitCard();
+    cardPlayer();
+    if ( playerScore >= 21 ) {
+        btnHit.attr('disabled',true);
+    }
+})
+
+
+
+
+
+
+btnStand.click( function () {
+
+    dealerPlays();
+    youWin();
+    
 })
